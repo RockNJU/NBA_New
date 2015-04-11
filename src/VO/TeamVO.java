7985d7831736ffc19173a5bf29bbd33731a22adc
2393147618@qq.com <wzce2393147618>
2015-04-11 18:53:41 +0800

    漏了TeamVOo(╯□╰)o

--- src/VO/TeamVO.java
+++ src/VO/TeamVO.java
@@ -9,12 +9,12 @@ public class TeamVO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	/*
 	 * 
-	 * ����ֻ���Զ��������޸�
+	 
 	 * 
 	 * 
 	 * */
-	private TeamInfoVO info;				//	��ӻ�����Ϣ
-	private TeamSeasonDataVO data;		//���������Ϣ
+	private TeamInfoVO info;				 
+	private TeamSeasonDataVO data;		 
 	
 	public  TeamVO(TeamInfoVO info,TeamSeasonDataVO data){
 		this.info=info;
