ec1c1a381645bb5ee9e58fb3bfa03df5d9dfbf64
wzce <2393147618@qq.com>
2015-04-25 19:20:48 +0800

    修改比赛中每一支球队的比赛王

--- src/VO/MatchVO.java
+++ src/VO/MatchVO.java
@@ -51,44 +51,87 @@ public class MatchVO implements Serializable{
 	public TeamMatchVO getGuestTeam() {
 		return guestTeam;
 	}
-	public ArrayList<SingleMatchPersonalDataVO> sort(String item){
+	private ArrayList<SingleMatchPersonalDataVO> H_sort(String item){
 		ArrayList<SingleMatchPersonalDataVO> list=hostTeam.getIndividualData();
-		list.addAll(guestTeam.getIndividualData());
+		//list.addAll(guestTeam.getIndividualData());
 		HotSort sort=new HotSort();
 		return sort.hotSinglePlayer_Sort(list, item);	
 	}
 	
-	public SingleMatchPersonalDataVO get_Highest_point(){
-		/*获取一场比赛中得分王*/
-		ArrayList<SingleMatchPersonalDataVO>list= sort("pointNum");
+	
+	private ArrayList<SingleMatchPersonalDataVO> G_sort(String item){
+		ArrayList<SingleMatchPersonalDataVO> list=guestTeam.getIndividualData();
+		//list.addAll(guestTeam.getIndividualData());
+		HotSort sort=new HotSort();
+		return sort.hotSinglePlayer_Sort(list, item);	
+	}
+	public SingleMatchPersonalDataVO get_GuestTeam_Highest_point(){
+		/*获取一场比赛中客队得分王*/
+		ArrayList<SingleMatchPersonalDataVO>list= G_sort("pointNum");
 		
 		return list.get(0);
 	}
 	
-	public SingleMatchPersonalDataVO get_Highest_rebound(){
-		/*获取一场比赛中篮板王*/
-		ArrayList<SingleMatchPersonalDataVO>list= sort("reboundNum");
+	public SingleMatchPersonalDataVO get_GuestTeam_Highest_rebound(){
+		/*获取一场比赛中客队篮板王*/
+		ArrayList<SingleMatchPersonalDataVO>list= G_sort("reboundNum");
 		
 		return list.get(0);
 	}
 	
-	public SingleMatchPersonalDataVO get_Highest_assist(){
-		/*获取一场比赛中的助攻王**/
-		ArrayList<SingleMatchPersonalDataVO>list= sort("assistNum");
+	public SingleMatchPersonalDataVO get_GuestTeam_Highest_assist(){
+		/*获取一场比赛中客队的助攻王**/
+		ArrayList<SingleMatchPersonalDataVO>list= G_sort("assistNum");
 		
 		return list.get(0);
 	}
 	
-	public SingleMatchPersonalDataVO get_Highest_steal(){
-		/*获取一场比赛中的抢断王*/
-		ArrayList<SingleMatchPersonalDataVO>list= sort("stealNum");
+	public SingleMatchPersonalDataVO get_GuestTeam_Highest_steal(){
+		/*获取一场比赛中的客队抢断王*/
+		ArrayList<SingleMatchPersonalDataVO>list= G_sort("stealNum");
 		
 		return list.get(0);
 	}
 	
-	public SingleMatchPersonalDataVO get_Highest_block(){
-		/*获取一场比赛中的盖帽王*/
-		ArrayList<SingleMatchPersonalDataVO>list= sort("blockNum");
+	public SingleMatchPersonalDataVO get_GuestTeam_Highest_block(){
+		/*获取一场比赛中的客队盖帽王*/
+		ArrayList<SingleMatchPersonalDataVO>list= G_sort("blockNum");
+		
+		return list.get(0);
+	}
+	
+	///////////////
+	public SingleMatchPersonalDataVO get_HostTeam_Highest_point(){
+		/*获取一场比赛中主队得分王*/
+		ArrayList<SingleMatchPersonalDataVO>list= H_sort("pointNum");
+		
+		return list.get(0);
+	}
+	
+	public SingleMatchPersonalDataVO get_HostTeam_Highest_rebound(){
+		/*获取一场比赛中主队篮板王*/
+		ArrayList<SingleMatchPersonalDataVO>list= H_sort("reboundNum");
+		
+		return list.get(0);
+	}
+	
+	public SingleMatchPersonalDataVO get_HostTeam_Highest_assist(){
+		/*获取一场比赛中的主队助攻王**/
+		ArrayList<SingleMatchPersonalDataVO>list= H_sort("assistNum");
+		
+		return list.get(0);
+	}
+	
+	public SingleMatchPersonalDataVO get_HostTeam_Highest_steal(){
+		/*获取一场比赛中的主队抢断王*/
+		ArrayList<SingleMatchPersonalDataVO>list= H_sort("stealNum");
+		
+		return list.get(0);
+	}
+	
+	public SingleMatchPersonalDataVO get_HostTeam_Highest_block(){
+		/*获取一场比赛中的主队盖帽王*/
+		ArrayList<SingleMatchPersonalDataVO>list= H_sort("blockNum");
 		
 		return list.get(0);
 	}
